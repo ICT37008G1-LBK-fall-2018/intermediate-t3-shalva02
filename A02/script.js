8f0dc96fcdd26aae123c2bbf51c521dbb8bb1da9
LumixGE <shotoo1@live.com>
2018-12-08 11:48:45 +0400

    exam

--- A02/script.js
+++ A02/script.js
@@ -1,15 +1,16 @@
 document.getElementById("highlight-product").onclick = function(){
-	var x = prompt('brendi')
+	var x = prompt('ბრენდი')
+	var y = prompt('შრიფტის ზომა')
 	var status = 0
 	for (var i = 0; i < document.getElementsByClassName('phone-list-item').length; i++) {
 		if (document.getElementsByClassName('phone-list-item')[i].getAttribute('data-brand') == x) {
 			status = 1
-			document.getElementsByClassName('phone-list-item')[i].children[0].style.fontSize = '40px'
+            document.getElementsByClassName('phone-list-item')[i].children[0].style.fontSize = y + 'px'
 			document.getElementsByClassName('phone-list-item')[i].children[0].innerHTML = document.getElementsByClassName('phone-list-item')[i].children[0].innerHTML.bold()
 
-            document.getElementsByClassName('phone-list-item')[i].children[1].style.fontSize = '40px'
-			document.getElementsByClassName('phone-list-item')[i].children[1].innerHTML = document.getElementsByClassName('phone-list-item')[i].children[1].innerHTML.bold()
-            
+            document.getElementsByClassName('phone-list-item')[i].children[1].style.fontSize = y + 'px'
+            document.getElementsByClassName('phone-list-item')[i].children[1].innerHTML = document.getElementsByClassName('phone-list-item')[i].children[1].innerHTML.bold()
+
 		}
 	}
 
@@ -17,4 +18,4 @@ document.getElementById("highlight-product").onclick = function(){
 	if (status == 0) {
 		alert('ჩანაწერი ვერ მოიძებნა')
 	}
-} 
+}
